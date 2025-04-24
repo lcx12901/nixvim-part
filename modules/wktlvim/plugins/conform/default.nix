e2095ee6f7d315307b076e2c21e1feb247fdb485
linchengxu <linchengxu@z9yun.com>
2025-04-24 18:50:28 +0800

    conform: remove eslint_d

--- modules/wktlvim/plugins/conform/default.nix
+++ modules/wktlvim/plugins/conform/default.nix
@@ -47,14 +47,6 @@
         toml = [ "taplo" ];
         yaml = [ "yamlfmt" ];
         sql = [ "sqlfluff" ];
-        json = [ "eslint_d" ];
-        jsonc = [ "eslint_d" ];
-        typescript = [ "eslint_d" ];
-        javascript = [ "eslint_d" ];
-        typescriptreact = [ "eslint_d" ];
-        vue = [ "eslint_d" ];
-        css = [ "eslint_d" ];
-        scss = [ "eslint_d" ];
       };
 
       formatters = {
