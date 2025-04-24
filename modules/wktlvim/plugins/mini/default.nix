4b8ab5ef4ae46d42e84efae36035f7c83ef7c532
lcx12901 <wktl1991504424@gmail.com>
2025-04-25 01:57:54 +0800

    mini: add snippets module

--- modules/wktlvim/plugins/mini/default.nix
+++ modules/wktlvim/plugins/mini/default.nix
@@ -1,3 +1,4 @@
+{ lib, config, ... }:
 {
   plugins = {
     mini = {
@@ -10,6 +11,14 @@
         basics = { };
         bracketed = { };
         icons = { };
+        snippets = {
+          snippets = {
+            __unkeyed-1.__raw =
+              lib.mkIf config.plugins.friendly-snippets.enable # Lua
+                "require('mini.snippets').gen_loader.from_file('${config.plugins.friendly-snippets.package}/snippets/global.json')";
+            __unkeyed-2.__raw = "require('mini.snippets').gen_loader.from_lang()";
+          };
+        };
       };
     };
   };
