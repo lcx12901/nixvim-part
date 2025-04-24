810e50a11cc3f678857568d06f4852555ea1dcd6
lcx12901 <wktl1991504424@gmail.com>
2025-04-25 01:58:29 +0800

    feat: add copilot-lua

---
+++ modules/wktlvim/plugins/copilot/default.nix
@@ -0,0 +1,134 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  plugins = {
+    copilot-lua = {
+      enable = true;
+
+      lazyLoad.settings.event = [ "InsertEnter" ];
+
+      settings = {
+        panel.enabled = !config.plugins.blink-cmp-copilot.enable;
+        suggestion.enabled = !config.plugins.blink-cmp-copilot.enable;
+        lsp_binary = lib.getExe pkgs.copilot-language-server;
+      };
+    };
+
+    copilot-chat = {
+      inherit (config.plugins.copilot-lua) enable;
+
+      lazyLoad.settings.cmd = [
+        "CopilotChat"
+        "CopilotChatAgents"
+        "CopilotChatLoad"
+        "CopilotChatModels"
+        "CopilotChatOpen"
+        "CopilotChatToggle"
+      ];
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.copilot-chat.enable [
+      {
+        __unkeyed-1 = "<leader>aC";
+        group = "Copilot";
+        icon = "";
+      }
+    ];
+  };
+
+  keymaps = lib.mkIf config.plugins.copilot-chat.enable [
+    {
+      mode = "n";
+      key = "<leader>aCc";
+      action = "<cmd>CopilotChat<CR>";
+      options = {
+        desc = "Open Chat";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCq";
+      action.__raw = ''
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end
+        end
+      '';
+      options = {
+        desc = "Quick Chat";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCh";
+      action.__raw = ''
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+        end
+      '';
+      options = {
+        desc = "Help Actions";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCp";
+      action.__raw = ''
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end
+      '';
+      options = {
+        desc = "Prompt Actions";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCa";
+      action = "<cmd>CopilotChatAgents<CR>";
+      options = {
+        desc = "List Available Agents";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCl";
+      action = "<cmd>CopilotChatLoad<CR>";
+      options = {
+        desc = "Load Chat History";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCm";
+      action = "<cmd>CopilotChatModels<CR>";
+      options = {
+        desc = "List Available Models";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCo";
+      action = "<cmd>CopilotChatOpen<CR>";
+      options = {
+        desc = "Open Chat Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aCt";
+      action = "<cmd>CopilotChatToggle<CR>";
+      options = {
+        desc = "Toggle Chat Window";
+      };
+    }
+  ];
+}
