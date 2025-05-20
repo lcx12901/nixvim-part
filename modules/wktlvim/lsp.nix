002ae7285911947934c4e4a8db731ba2039f0b98
lcx12901 <wktl1991504424@gmail.com>
2025-05-20 21:56:35 +0800

    lsp: keymaps

--- modules/wktlvim/lsp.nix
+++ modules/wktlvim/lsp.nix
@@ -1,3 +1,4 @@
+{ lib, config, ... }:
 {
   imports = [
     ./lsp/eslint.nix
@@ -36,4 +37,45 @@
       volar.enable = true;
     };
   };
+
+  keymapsOnEvents.LspAttach =
+    [
+      (lib.mkIf (!config.plugins.conform-nvim.enable) {
+        action.__raw = ''vim.lsp.buf.format'';
+        mode = "v";
+        key = "<leader>lf";
+        options = {
+          silent = true;
+          buffer = false;
+          desc = "Format selection";
+        };
+      })
+      # Diagnostic keymaps
+      {
+        key = "<leader>lH";
+        mode = "n";
+        action = lib.nixvim.mkRaw "vim.diagnostic.open_float";
+        options = {
+          silent = true;
+          desc = "Lsp diagnostic open_float";
+        };
+      }
+    ]
+    ++ lib.optionals
+      (
+        !config.plugins.fzf-lua.enable
+        || (config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings)
+      )
+      [
+        # Code action keymap (if fzf-lua is not enabled)
+        {
+          key = "<leader>la";
+          mode = "n";
+          action = lib.nixvim.mkRaw "vim.lsp.buf.code_action";
+          options = {
+            silent = true;
+            desc = "Lsp buf code_action";
+          };
+        }
+      ];
 }
