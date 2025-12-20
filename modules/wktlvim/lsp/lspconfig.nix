f8f136da8f9f3b414617b433c37a799bb1107816
lcx12901 <wktl1991504424@gmail.com>
2025-12-21 01:18:56 +0800

    lspconfig: remove redundant border config
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/wktlvim/lsp/lspconfig.nix
+++ modules/wktlvim/lsp/lspconfig.nix
@@ -1,16 +1,3 @@
 {
-  config,
-  lib,
-  ...
-}:
-{
-  extraConfigLuaPre = lib.mkIf config.plugins.lspconfig.enable ''
-    require('lspconfig.ui.windows').default_options = {
-      border = "rounded"
-    }
-  '';
-
-  plugins = {
-    lspconfig.enable = true;
-  };
+  plugins.lspconfig.enable = true;
 }
