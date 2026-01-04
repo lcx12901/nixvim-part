390f89e6a3358d29a00c6ac8f98f82cba0867a07
lcx12901 <wktl1991504424@gmail.com>
2026-01-04 23:54:52 +0800

    treesitter: grammarPackages list
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/wktlvim/plugins/treesitter/default.nix
+++ modules/wktlvim/plugins/treesitter/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ pkgs, ... }:
 {
   plugins = {
     treesitter = {
@@ -6,51 +6,41 @@
 
       folding.enable = true;
 
-      grammarPackages =
-        let
-          # Large grammars that are not used
-          excludedGrammars = [
-            "tree-sitter-agda"
-            "tree-sitter-apex"
-            "tree-sitter-arduino"
-            "tree-sitter-blade"
-            "tree-sitter-c3"
-            "tree-sitter-cuda"
-            "tree-sitter-d"
-            "tree-sitter-elixir"
-            "tree-sitter-fortran"
-            "tree-sitter-gnuplot"
-            "tree-sitter-hack"
-            "tree-sitter-haskell"
-            "tree-sitter-hlsl"
-            "tree-sitter-hoon"
-            "tree-sitter-idris"
-            "tree-sitter-ispc"
-            "tree-sitter-julia"
-            "tree-sitter-kotlin"
-            "tree-sitter-koto"
-            "tree-sitter-latex"
-            "tree-sitter-lean"
-            "tree-sitter-nim"
-            "tree-sitter-ocaml"
-            "tree-sitter-ocaml_interface"
-            "tree-sitter-odin"
-            "tree-sitter-perl"
-            "tree-sitter-purescript"
-            "tree-sitter-qmljs"
-            "tree-sitter-razor"
-            "tree-sitter-ruby"
-            "tree-sitter-scala"
-            "tree-sitter-slang"
-            "tree-sitter-systemverilog"
-            "tree-sitter-tlaplus"
-            "tree-sitter-unison"
-            "tree-sitter-v"
-            "tree-sitter-verilog"
-            "tree-sitter-vhdl"
-          ];
-        in
-        lib.filter (g: !(lib.elem g.pname excludedGrammars)) config.plugins.treesitter.package.allGrammars;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        css
+        diff
+        dockerfile
+        git_config
+        git_rebase
+        gitattributes
+        gitcommit
+        gitignore
+        html
+        http
+        ini
+        javascript
+        json
+        kdl
+        lua
+        markdown
+        markdown_inline
+        nginx
+        nix
+        python
+        query
+        regex
+        rust
+        scss
+        toml
+        tsx
+        typescript
+        vim
+        vimdoc
+        vue
+        xml
+        yaml
+      ];
       nixvimInjections = true;
 
       settings = {
