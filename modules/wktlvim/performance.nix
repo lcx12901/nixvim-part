5372eccb50ab5e9ac325445afbf7128f716a9400
lcx12901 <wktl1991504424@gmail.com>
2026-01-07 22:53:04 +0800

    treesitter: tweaks
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/wktlvim/performance.nix
+++ modules/wktlvim/performance.nix
@@ -11,12 +11,8 @@
 
     combinePlugins = {
       standalonePlugins = with pkgs.vimPlugins; [
-        "firenvim"
-        "neotest"
         "nvim-treesitter"
         mini-nvim
-        overseer-nvim
-        vs-tasks-nvim
         "friendly-snippets" # needed for blink to access friendly-snippets
       ];
     };
--- modules/wktlvim/plugins/treesitter/default.nix
+++ modules/wktlvim/plugins/treesitter/default.nix
@@ -1,73 +1,61 @@
-{ pkgs, ... }:
+{ config, lib, ... }:
 {
   plugins = {
     treesitter = {
       enable = true;
 
       folding.enable = true;
+      highlight.enable = true;
+      indent.enable = true;
 
-      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        bash
-        css
-        diff
-        dockerfile
-        git_config
-        git_rebase
-        gitattributes
-        gitcommit
-        gitignore
-        html
-        http
-        ini
-        javascript
-        json
-        kdl
-        lua
-        markdown
-        markdown_inline
-        nginx
-        nix
-        python
-        query
-        regex
-        rust
-        scss
-        toml
-        tsx
-        typescript
-        vim
-        vimdoc
-        vue
-        xml
-        yaml
-      ];
+      grammarPackages =
+        let
+          excludedGrammars = [
+            # Size
+            "tree-sitter-agda"
+            "tree-sitter-apex"
+            "tree-sitter-arduino"
+            "tree-sitter-blade"
+            "tree-sitter-c3"
+            "tree-sitter-cuda"
+            "tree-sitter-d"
+            "tree-sitter-elixir"
+            "tree-sitter-fortran"
+            "tree-sitter-gnuplot"
+            "tree-sitter-hack"
+            "tree-sitter-haskell"
+            "tree-sitter-hlsl"
+            "tree-sitter-hoon"
+            "tree-sitter-idris"
+            "tree-sitter-ispc"
+            "tree-sitter-julia"
+            "tree-sitter-kotlin"
+            "tree-sitter-koto"
+            "tree-sitter-latex"
+            "tree-sitter-lean"
+            "tree-sitter-nim"
+            "tree-sitter-ocaml"
+            "tree-sitter-ocaml_interface"
+            "tree-sitter-odin"
+            "tree-sitter-perl"
+            "tree-sitter-purescript"
+            "tree-sitter-qmljs"
+            "tree-sitter-razor"
+            "tree-sitter-ruby"
+            "tree-sitter-scala"
+            "tree-sitter-slang"
+            "tree-sitter-systemverilog"
+            "tree-sitter-tlaplus"
+            "tree-sitter-unison"
+            "tree-sitter-v"
+            "tree-sitter-verilog"
+            "tree-sitter-vhdl"
+
+            # Never going to use
+          ];
+        in
+        lib.filter (g: !(lib.elem g.pname excludedGrammars)) config.plugins.treesitter.package.allGrammars;
       nixvimInjections = true;
-
-      settings = {
-        highlight = {
-          additional_vim_regex_highlighting = true;
-          enable = true;
-          disable = ''
-            function(lang, bufnr)
-              return vim.api.nvim_buf_line_count(bufnr) > 10000
-            end
-          '';
-        };
-
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "<A-o>";
-            node_incremental = "<A-o>";
-            scope_incremental = "<A-O>";
-            node_decremental = "<A-i>";
-          };
-        };
-
-        indent = {
-          enable = true;
-        };
-      };
     };
   };
 }
