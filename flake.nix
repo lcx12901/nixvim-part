729c5f68518240c525ea096bc30f3499c9b553db
lcx12901 <wktl1991504424@gmail.com>
2026-01-14 23:42:53 +0800

    only x86_64 system
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- flake.nix
+++ flake.nix
@@ -39,10 +39,7 @@
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "aarch64-linux"
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       imports = [
         ./flake
