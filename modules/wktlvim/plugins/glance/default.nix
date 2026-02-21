fcc68a1d02b2ff4389fb78e21898dcb1ea2b5dcc
lcx12901 <wktl1991504424@gmail.com>
2026-02-21 20:13:01 +0800

    feat(glance): add module
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

---
+++ modules/wktlvim/plugins/glance/default.nix
@@ -0,0 +1,49 @@
+_: {
+  plugins = {
+    glance = {
+      enable = true;
+
+      lazyLoad.settings.cmd = "Glance";
+
+      settings = {
+        border.enable = true;
+      };
+    };
+
+  };
+
+  keymaps = [
+    {
+      action = "<CMD>Glance definitions<CR>";
+      mode = "n";
+      key = "<leader>ld";
+      options = {
+        desc = "Glance definition";
+      };
+    }
+    {
+      action = "<CMD>Glance implementations<CR>";
+      mode = "n";
+      key = "<leader>li";
+      options = {
+        desc = "Glance implementation";
+      };
+    }
+    {
+      action = "<CMD>Glance references<CR>";
+      mode = "n";
+      key = "<leader>lD";
+      options = {
+        desc = "Glance reference";
+      };
+    }
+    {
+      action = "<CMD>Glance type_definitions<CR>";
+      mode = "n";
+      key = "<leader>lt";
+      options = {
+        desc = "Glance type definition";
+      };
+    }
+  ];
+}
