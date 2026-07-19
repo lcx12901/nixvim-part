57d65ef46581db65ba1fa5a144276f914a2733b6
lcx12901 <wktl1991504424@gmail.com>
2026-07-19 23:11:11 +0800

    feat(plugins): add git-worktree plugin
    Add git worktree management with switch/delete hooks and notifications.

---
+++ modules/wktlvim/plugins/git-worktree/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  # See: https://github.com/ThePrimeagen/git-worktree.nvim
+  plugins.git-worktree = {
+    enable = true;
+
+    settings = {
+      change_directory_command = "cd";
+      update_on_change = true;
+      update_on_change_command = "e .";
+    };
+  };
+
+  extraConfigLua = lib.mkIf config.plugins.git-worktree.enable ''
+    local Hooks = require("git-worktree.hooks")
+    local git_worktree_config = require('git-worktree.config')
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+    Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+      vim.notify("Moved from " .. prev_path .. " to " .. path)
+      update_on_switch(path, prev_path)
+    end)
+
+    Hooks.register(Hooks.type.DELETE, function()
+      vim.cmd(git_worktree_config.update_on_change_command)
+    end)
+  '';
+}
